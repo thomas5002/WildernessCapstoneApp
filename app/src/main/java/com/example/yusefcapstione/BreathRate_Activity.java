bcdcc3b99d6cea4fbcb5266395f5e4b6bde518c9
thomas5002 <thomas.pinsonneault@gmail.com>
2023-11-12 11:56:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yusefcapstione/BreathRate_Activity.java
@@ -0,0 +1,33 @@
+package com.example.yusefcapstione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BreathRate_Activity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_rate);
+
+        button = findViewById(R.id.backBTN);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BreathRate_Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/yusefcapstione/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.yusefcapstione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button HRBTN , BRBTN, GRAPHBTN, SETTINGSBTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HRBTN = findViewById(R.id.hrBTN);
+        BRBTN = findViewById(R.id.brBTN);
+        GRAPHBTN = findViewById(R.id.GraphBTN);
+        SETTINGSBTN = findViewById(R.id.SettingBTN);
+
+        HRBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HeartRate_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        BRBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BreathRate_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        GRAPHBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Graph_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        SETTINGSBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
