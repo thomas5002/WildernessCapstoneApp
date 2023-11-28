0f98e248df3938388a2394e10b4e9f56229337ae
MojoJojo2296 <rejalashaw@cmail.carleton.ca>
2023-11-28 16:47:08 -0500

    added functional database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services") //new
 }
 
 android {
@@ -20,8 +21,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
         }
     }
@@ -35,12 +36,15 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0")) //new
+    implementation("com.google.firebase:firebase-analytics") //new
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.7.4")
-    implementation("androidx.navigation:navigation-ui:2.7.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/yusefcapstione/MainActivity.java
+++ app/src/main/java/com/example/yusefcapstione/MainActivity.java
@@ -1,20 +1,66 @@
 package com.example.yusefcapstione;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private Button HRBTN , BRBTN, GRAPHBTN, SETTINGSBTN;
+    private Button HRBTN , BRBTN, GRAPHBTN, SETTINGSBTN, btn, btnRead;
+    private EditText test;
+    private TextView textView3;
+    private DatabaseReference rootDatabaseref;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = findViewById(R.id.test);
+        btn = findViewById(R.id.btn);
+        btnRead = findViewById(R.id.btnRead);
+        textView3 = findViewById(R.id.textView3);
 
+        rootDatabaseref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootDatabaseref.child("user1").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String data = dataSnapshot.getValue().toString();
+                            textView3.setText(data);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data=test.getText().toString();
+                rootDatabaseref.child("user1").setValue(data);
+            }
+        });
         HRBTN = findViewById(R.id.hrBTN);
         BRBTN = findViewById(R.id.brBTN);
         GRAPHBTN = findViewById(R.id.GraphBTN);
