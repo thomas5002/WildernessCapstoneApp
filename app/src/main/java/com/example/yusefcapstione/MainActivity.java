eb1d6b6bb4b5ac5435a1b30a7b1525f7b1f16d15
kidnhoxz <vietdutran@cmail.carleton.ca>
2023-11-29 22:14:16 -0500

    Added Success & Failure Listener With Firebase Function

--- app/src/main/java/com/example/yusefcapstione/MainActivity.java
+++ app/src/main/java/com/example/yusefcapstione/MainActivity.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -55,7 +58,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String data=test.getText().toString();
-                rootDatabaseref.child("user1").setValue(data);
+                rootDatabaseref.child("user1").setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Your Data is successfully added", Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"Sorry! Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
         HRBTN = findViewById(R.id.hrBTN);
