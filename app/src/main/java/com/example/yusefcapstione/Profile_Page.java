de848e49d8446a59d3e95c194c41679e3f707fea
EmilioL53 <57446542+EmilioL53@users.noreply.github.com>
2024-01-06 18:57:42 -0500

    attempt to create a user file attached to inputted name

--- app/src/main/java/com/example/yusefcapstione/Profile_Page.java
+++ app/src/main/java/com/example/yusefcapstione/Profile_Page.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;  //stuff below is added so we can find the edit text so name can be manipulated
+import android.text.Editable;    //added for name change
+import android.text.TextWatcher; //added for name change
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile_Page extends AppCompatActivity {
 
     private Button LOADUSERBTN, INC1BTN;
-
+    private EditText editText; // Added line for name change
     public int visC = 0;
 
     @Override
@@ -20,22 +24,23 @@ public class Profile_Page extends AppCompatActivity {
 
         LOADUSERBTN = findViewById(R.id.loadUserBTN);
         INC1BTN = findViewById(R.id.inc1BTN);
+        editText = findViewById(R.id.test); // Added line for name change
 
         INC1BTN.setVisibility(View.INVISIBLE);           // starts the incidents button as hidden
 
         LOADUSERBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                visC ++;
+                visC++;
                 Intent intent = new Intent(Profile_Page.this, MainActivity.class);
                 startActivity(intent);
 
-                if(visC == 1){
+                if (visC == 1) {
                     INC1BTN.setVisibility(View.VISIBLE);
                 }
 
-            //    v.setVisibility(View.VISIBLE);
-            //    v.setVisibility(View.INVISIBLE);
+                //    v.setVisibility(View.VISIBLE);
+                //    v.setVisibility(View.INVISIBLE);
 
 
             }
@@ -49,9 +54,28 @@ public class Profile_Page extends AppCompatActivity {
             }
         });
 
+        // Added block for name change - Start
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This method is called before the text is changed
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // This method is called when the text is being changed
+                String enteredText = s.toString();
+                // Modify the button's text based on the entered text
+                INC1BTN.setText(enteredText);
+            }
 
-
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This method is called after the text has changed
+            }
+        });
+        // Added block - End
 
     }
+
 }
