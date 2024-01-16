d02403525eb78cbd61437785d28d125ded9d626b
Du Tran <dutran@Dus-Mac-mini.local>
2024-01-15 19:13:39 -0500

    Remove Read and Send button along with Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,15 +36,13 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0")) //new
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0")) //new
     implementation("com.google.firebase:firebase-analytics") //new
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.7.5")
-    implementation("androidx.navigation:navigation-ui:2.7.5")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
